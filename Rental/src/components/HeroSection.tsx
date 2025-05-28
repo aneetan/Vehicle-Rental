c76e8d826332c700106056697beb6716a428199c
aneetan <anitaneupane2061@gmail.com>
2025-05-28 20:00:13 +0545

    Hero-section with form

--- Rental/src/components/HeroSection.tsx
+++ Rental/src/components/HeroSection.tsx
@@ -1,25 +1,41 @@
-
+import Image from '../assets/images/car.png'
 const HeroSection = () => {
   return (
-    <>
-        <section className="bg-gray-400 text-white py-20 z-[-100]">
-            <div className="flex justify-between items-center">
-                {/* Left Section */}
-                <div className="container w-1/2 px-6 m-10">
-                    <h1 className="text-4xl md:text-6xl font-bold mb-4">Welcome to Rent-All
-                    </h1>
-                    <p className="text-lg md:text-xl mb-8">Your one-stop solution for all vehicle rentals</p>
-                    <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
-                        Get Started
-                    </button>
-                </div>
-
-                {/* Right Section */}
-                <div className="w-1/2 hidden md:block">
-                    <img className="h-120" src="https://cdn.pixabay.com/photo/2022/07/04/10/46/vintage-car-7300881_1280.jpg"/>
+    <>  
+        <section className="relative min-h-[500px] bg-[#e6e0dc] overflow-hidden md:pt-30 items-center py-12">
+            {/* ------- Background shape ------- */}
+            <div className="absolute inset-0 overflow-hidden">
+                <div className=" absolute top-0 right-0 left-1/2 bg-[#f86c23] transform -skew-x-12 origin-top-left">
                 </div>
             </div>
-            
+
+            <div className="container mx-auto px-4 relative z-10 md:mx-12">
+                <div className="flex flex-col md:flex-row items-center">
+                    <div className="w-full md:w-[1/2] mb-8 md:mb-0">
+                        <h1 className="text-3xl md:text-5xl font-bold mb-4">Rent Your <br/> Dream Vehicle</h1>
+                        <p className="text-sm text-gray-500 md:text-lg mb-6">Explore our wide range of vehicles available for rent.</p>
+
+                        <div className="flex flex-wrap gap-4 mb-6">
+                            <button className="bg-[#f86c23] text-white px-6 py-3 rounded-lg font-semibold hover:bg-gray-100 transition">
+                                Rent Now
+                            </button>
+                            <button className="bg-transparent border-2 border-[#f86c23] text-[#f86c23] px-6 py-3 rounded-lg font-semibold hover:bg-white hover:text-[#F86C23] transition">
+                                Get Started
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="w-full hidden md:block md:w-[1/2] relative">
+                        <div className="relative md:absolute md:-right-20 md:-top-20">
+                            <img 
+                                src={Image}
+                                alt="Rental car" 
+                                className="w-full max-w-md mx-auto md:mx-0 transform hover:scale-105 transition duration-300"
+                            />             
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     </>
   )
--- Rental/src/components/HowItWorks.tsx
+++ Rental/src/components/HowItWorks.tsx
@@ -2,8 +2,8 @@
 const HowItWorks = () => {
   return (
     <>
-      <section className="bg-gray-100 py-16">
-        <div className="container mx-auto px-6">
+      <section className="bg-gray-100">
+        <div className="container mx-auto px-6 py-36">
           <h2 className="text-3xl font-bold text-center mb-8">How It Works</h2>
           
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8 items-center text-center">
